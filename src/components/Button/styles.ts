41bcc31432e3a05469776ec87850a8004a242757
Victor Buarque <vcgabriel38@gmail.com>
2024-04-01 17:16:15 -0300

    feat: create variant style

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,41 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { IButtonStyled } from './types'
 
 
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<IButtonStyled>`
     width: 100%;
     height: 42px;
-    background-color: #81259D;
+    background-color: #d3d3d3;
     color: #FFF;
 
-    border: 1px solid #81259D;
+    border: 1px solid #d3d3d3;
     border-radius: 21px;
 
     &:hover {
         opacity: 0.6;
         cursor:pointer;
     }
+
+    ${({ variant }) => variant !== "primary" && css`
+    min-width:167px;
+    height: 33px;
+    background: #81259D;
+    color: #FFFFFF;
+    border-radius:22px;
+    &:hover{
+        opacity:0.6;
+        cursor: pointer;
+    }
+    &::after{
+        content: '';
+        position: absolute;
+    //    border: 1px solid #81259D;
+        top: -5px;
+        left: -6px;
+        width: calc(100% + 10px);
+        height:calc(100% +10px);
+        border-radius:22px;
+        
+    }
+`}
 `
