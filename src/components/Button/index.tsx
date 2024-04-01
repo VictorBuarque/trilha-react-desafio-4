45ee3502bc7fc45400131476098433e6a6cc0b86
Victor Buarque <vcgabriel38@gmail.com>
2024-04-01 17:16:44 -0300

    feat: create disabled and variant

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,8 @@
 import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+const Button = ({ title, onClick, disabled, variant="primary" }: IButtonProps) => {
+  return <ButtonContainer onClick={onClick} disabled={disabled} variant={variant}>{title}</ButtonContainer>;
 };
 
 export default Button;
